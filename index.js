5e82df42a31f57742037d9cb3f7512549669dd42
Arjun Sharma <arjunsharma6622@gmail.com>
2026-02-03 08:48:36 +0000

    updating line after doing replatform

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Hello from POC15 Server, using rehost strategy x");
+    res.send("Hello from POC15 Server, using rehost strategy x, added line after doing replatforming");
 })
 
 const PORT = process.env.PORT;
